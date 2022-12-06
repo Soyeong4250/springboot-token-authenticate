0374b73ff38687675f5eddcc85eec2d93d99e23b
이소영 <th42500@naver.com>
2022-12-06 12:33:10 +0900

    [Feat] Spring Security 설정 추가

---
+++ src/main/java/com/token/authenticate/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.token.authenticate.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/v1/**").permitAll()
+                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
+
+}
