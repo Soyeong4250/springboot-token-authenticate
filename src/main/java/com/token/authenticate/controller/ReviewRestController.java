f04cfa48077ee23e80794430e29fde6a53446951
이소영 <th42500@naver.com>
2022-12-06 14:52:28 +0900

    [Refactor] 리뷰 등록 시 "~님의 리뷰가 등록되었습니다" 뜨도록 수정
    1. ReviewService 추가
    2. ReviewRestController - ReviewService를 통해 반환한 문자열을 Response에 담아 return

--- src/main/java/com/token/authenticate/controller/ReviewRestController.java
+++ src/main/java/com/token/authenticate/controller/ReviewRestController.java
@@ -1,7 +1,10 @@
 package com.token.authenticate.controller;
 
 import com.token.authenticate.domain.dto.ReviewCreateReq;
+import com.token.authenticate.service.ReviewService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,13 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/v1/reviews")
 public class ReviewRestController {
 
+    private final ReviewService reviewService;
+
     @PostMapping
-    public String write(@RequestBody ReviewCreateReq request, Authentication authentication) {
+    public ResponseEntity<String> write(@RequestBody ReviewCreateReq request, Authentication authentication) {
         log.info("writer : {}", authentication.getName());
-        return "리뷰 등록 성공";
+        return ResponseEntity.ok()
+                .body(reviewService.write(authentication.getName()));
     }
 }
