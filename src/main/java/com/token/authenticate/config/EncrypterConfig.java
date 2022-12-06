0a8a86a320c6dc2fbd538c8125efddc771b591f3
이소영 <th42500@naver.com>
2022-12-06 12:33:36 +0900

    [Feat] 비밀번호 암호화를 위한 EncrypterConfig 설정 추가

---
+++ src/main/java/com/token/authenticate/config/EncrypterConfig.java
@@ -0,0 +1,14 @@
+package com.token.authenticate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class EncrypterConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+}
